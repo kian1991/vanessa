9e06fbebfd28455ca80922f6f6bfa33c7ec883dc
kian1991 <kianluetke@gmail.com>
2021-06-13 16:13:37 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,54 @@
+import PyPDF2
+import re
+
+PAGE_OFFSET = - 6
+
+umlaute = {'ä': 'Ñ', 'ü': 'Õ', 'ö': 'Ó', 'ß': 'û'}
+ignore_list = ['der', 'das', 'die', '#', ' - ']
+skip_list = ['siehe', 'auch']
+
+# Open the pdf file
+pdf = PyPDF2.PdfFileReader("decrypted.pdf")
+
+# Get number of pages
+num_pages = pdf.getNumPages()
+
+
+
+new_line = '';
+# Enter code here
+strings = ["Angestelltensubjekt", "siehe Subjekt"]
+
+# check skip list
+for index, string in enumerate(strings):
+    for item in skip_list:
+        if item in string:
+            if len(strings) > 1:
+                del strings[index]
+
+# Replace Umlaute
+for key, value in umlaute.items():
+    strings = [string.replace(key, value) for string in strings]
+# ignore words or characters
+for item in ignore_list:
+    strings = [string.replace(item, '') for string in strings]
+# strip whitespaces
+strings = [string.replace(' ', '') for string in strings]
+print(strings)
+results = []
+
+# Extract text and do the search
+for i in range(0, num_pages):
+    page_obj = pdf.getPage(i)
+    text = page_obj.extractText()
+    for string in strings:
+        if re.search(string.lower(),text.lower()):
+            results.append(str(i + PAGE_OFFSET))
+
+result_string = ''
+for x in results:
+    result_string += f'{x}, '
+result_string = result_string[:-2]
+
+new_line += f'TEST  {result_string}\n'
+print(new_line)
